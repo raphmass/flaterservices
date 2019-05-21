ffa9bd5a134a8072e1daad0567bdb454d261f692
Maxime <maxime.forler1@gmail.com>
2019-05-21 14:16:27 +0200

    ADD SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,36 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # seeding database
+require 'faker'
 
+puts "seed start :-)"
 
+User.destroy_all
+Task.destroy_all
 
+puts "creating users"
 100.times do
-  User.create(
-    first_name: Faker::Name.first_name
-    last_name: Faker::Name.last_name
-    email: Faker::Internet.email
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
   )
 end
+puts "users created!"
 
+users = User.all
+
+puts "creating tasks"
 100.times do
-  Task.create(
-    action: ['Visit', 'Cleaning', 'Check-in/out'].sample
-    status: 'Available'
-    price: (5..50).to_a.sample
+  Task.create!(
+    action: ['Visit', 'Cleaning', 'Check-in/out'].sample,
+    status: 'Available',
+    price: (5..50).to_a.sample,
+    user: users.sample
   )
 end
+puts "tasks created!"
 
 
 
