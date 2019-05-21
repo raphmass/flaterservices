18735b41a1565e5cb7c8347b484e7ef0776ce216
Pierre M'baga <pierrembaga@gmail.com>
2019-05-21 18:37:22 +0200

    migration for Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  ACTIONS = ['Visit', 'Cleaning', 'Check-in/out']
+  ACTIONS = ['Visit', 'Cleaning', 'Check-in/out', 'extra']
   STATUS = ['Not assigned', 'Assigned']
 end
