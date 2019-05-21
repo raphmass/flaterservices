d4f3015fc1f14b1fcd00c67ac78f44c986340735
Maxime <maxime.forler1@gmail.com>
2019-05-21 12:46:04 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seeding database
+
+
+
+100.times do
+  User.create(
+    first_name: Faker::Name.first_name
+    last_name: Faker::Name.last_name
+    email: Faker::Internet.email
+  )
+end
+
+100.times do
+  Task.create(
+    action: ['Visit', 'Cleaning', 'Check-in/out'].sample
+    status: 'Available'
+    price: (5..50).to_a.sample
+  )
+end
+
+
+
