7bd2ef041616afab9e2c9aaee1068b68913d5f66
Pierre M'baga <pierrembaga@gmail.com>
2019-05-22 17:55:31 +0200

    ADD: migrate Task Long/Lat AND Geocoder map

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 
   ACTIONS = ['visit', 'cleaning', 'check-in/out', 'extra']
   STATUS = ['not assigned', 'assigned', 'in progress', 'done']
