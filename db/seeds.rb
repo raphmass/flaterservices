7dd2f4f43403ff7f84d9b262ef0888ab307eb6bb
Dareth NHANG <hello@dsigned.me>
2019-05-23 14:30:05 +0200

    FIX: Seed random addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,46 +45,47 @@ end
   )
 end
 
-address = [
-  {street_number: '5', street_name: 'Rue Trarieux', code_post: '69003', city: 'Lyon'},
-  {street_number: '18', street_name: 'Rue Franklin', code_post: '69002', city: 'Lyon'},
-  {street_number: '58', street_name: 'Rue Pasteur', code_post: '69007', city: 'Lyon'},
-  {street_number: '22', street_name: 'Rue Smith', code_post: '69002', city: 'Lyon'},
-  {street_number: '32', street_name: 'Avenue Felix Faure', code_post: '69003', city: 'Lyon'},
-  {street_number: '8', street_name: 'Rue Rollet', code_post: '69003', city: 'Lyon'},
-  {street_number: '52', street_name: 'Rue de Sèze', code_post: '69006', city: 'Lyon'},
-  {street_number: '10', street_name: 'Rue Mazenod', code_post: '69003', city: 'Lyon'},
-  {street_number: '30', street_name: 'Rue Cuvier', code_post: '69006', city: 'Lyon'},
-  {street_number: '72', street_name: 'Boulevard Vivier Merle', code_post: '69003', city: 'Lyon'},
-  {street_number: '45', street_name: 'Rue de la république', code_post: '69002', city: 'Lyon'},
-  {street_number: '67', street_name: 'Rue du Président Edouard Herriot', code_post: '69003', city: 'Lyon'},
-  {street_number: '98', street_name: 'Rue Paul Bert', code_post: '69003', city: 'Lyon'},
-  {street_number: '20', street_name: 'Rue des capucins', code_post: '69001', city: 'Lyon'},
-  {street_number: '3', street_name: 'Avenue Lacassagne', code_post: '69003', city: 'Lyon'},
-  {street_number: '132', street_name: 'Avenue Lacassagne', code_post: '69003', city: 'Lyon'},
-  {street_number: '48', street_name: 'Rue Garibaldi ', code_post: '69003', city: 'Lyon'},
-  {street_number: '10', street_name: 'Rue de la charité', code_post: '69002', city: 'Lyon'},
-  {street_number: '57', street_name: 'Rue de Marseille', code_post: '69007', city: 'Lyon'},
-  {street_number: '3', street_name: 'Rue Chevreul', code_post: '69007', city: 'Lyon'},
-  {street_number: '12', street_name: 'Rue de la Thibaudière', code_post: '69007', city: 'Lyon'},
-  {street_number: '3', street_name: 'Rue Père Chevrier', code_post: '69007', city: 'Lyon'},
-  {street_number: '7', street_name: 'Rue du Plat', code_post: '69002', city: 'Lyon'},
-  {street_number: '9', street_name: 'Rue de Condé', code_post: '69002', city: 'Lyon'},
-  {street_number: '5', street_name: 'Rue Servient', code_post: '69003', city: 'Lyon'},
-  {street_number: '65', street_name: 'Rue Bossuet', code_post: '69006', city: 'Lyon'},
-  {street_number: '12', street_name: 'Rue de la république', code_post: '69002', city: 'Lyon'},
-  {street_number: '45', street_name: 'Rue Trarieux', code_post: '69003', city: 'Lyon'}
-]
 puts "... users created!"
 
 puts separator
 puts "2. Creating random tasks... "
 users = User.first(25)
+addresses = [
+  {street_number: '5', street_name: 'Rue Trarieux', zipcode: '69003', city: 'Lyon'},
+  {street_number: '18', street_name: 'Rue Franklin', zipcode: '69002', city: 'Lyon'},
+  {street_number: '58', street_name: 'Rue Pasteur', zipcode: '69007', city: 'Lyon'},
+  {street_number: '22', street_name: 'Rue Smith', zipcode: '69002', city: 'Lyon'},
+  {street_number: '32', street_name: 'Avenue Felix Faure', zipcode: '69003', city: 'Lyon'},
+  {street_number: '8', street_name: 'Rue Rollet', zipcode: '69003', city: 'Lyon'},
+  {street_number: '52', street_name: 'Rue de Sèze', zipcode: '69006', city: 'Lyon'},
+  {street_number: '10', street_name: 'Rue Mazenod', zipcode: '69003', city: 'Lyon'},
+  {street_number: '30', street_name: 'Rue Cuvier', zipcode: '69006', city: 'Lyon'},
+  {street_number: '72', street_name: 'Boulevard Vivier Merle', zipcode: '69003', city: 'Lyon'},
+  {street_number: '45', street_name: 'Rue de la république', zipcode: '69002', city: 'Lyon'},
+  {street_number: '67', street_name: 'Rue du Président Edouard Herriot', zipcode: '69003', city: 'Lyon'},
+  {street_number: '98', street_name: 'Rue Paul Bert', zipcode: '69003', city: 'Lyon'},
+  {street_number: '20', street_name: 'Rue des capucins', zipcode: '69001', city: 'Lyon'},
+  {street_number: '3', street_name: 'Avenue Lacassagne', zipcode: '69003', city: 'Lyon'},
+  {street_number: '132', street_name: 'Avenue Lacassagne', zipcode: '69003', city: 'Lyon'},
+  {street_number: '48', street_name: 'Rue Garibaldi ', zipcode: '69003', city: 'Lyon'},
+  {street_number: '10', street_name: 'Rue de la charité', zipcode: '69002', city: 'Lyon'},
+  {street_number: '57', street_name: 'Rue de Marseille', zipcode: '69007', city: 'Lyon'},
+  {street_number: '3', street_name: 'Rue Chevreul', zipcode: '69007', city: 'Lyon'},
+  {street_number: '12', street_name: 'Rue de la Thibaudière', zipcode: '69007', city: 'Lyon'},
+  {street_number: '3', street_name: 'Rue Père Chevrier', zipcode: '69007', city: 'Lyon'},
+  {street_number: '7', street_name: 'Rue du Plat', zipcode: '69002', city: 'Lyon'},
+  {street_number: '9', street_name: 'Rue de Condé', zipcode: '69002', city: 'Lyon'},
+  {street_number: '5', street_name: 'Rue Servient', zipcode: '69003', city: 'Lyon'},
+  {street_number: '65', street_name: 'Rue Bossuet', zipcode: '69006', city: 'Lyon'},
+  {street_number: '12', street_name: 'Rue de la république', zipcode: '69002', city: 'Lyon'},
+  {street_number: '45', street_name: 'Rue Trarieux', zipcode: '69003', city: 'Lyon'}
+]
 100.times do
+  address = addresses.sample
   Task.create!(
     action: Task::ACTIONS.sample,
     status: (0..(Task::STATUS.size - 1)).to_a.sample,
-    location: address.sample
+    location: "#{address[:street_number]} #{address[:street_name]}, #{address[:zipcode]} #{address[:city]}",
     price: (5..50).to_a.sample,
     user: users.sample
   )
@@ -112,10 +113,13 @@ max = User.find_by(email: 'max@gmail.com')
 
 dareth.tasks.destroy_all
 
+address = addresses.sample
+
 t1 = Task.create!(
   action: Task::ACTIONS.sample,
   status: 1, # In progress
   price: (5..50).to_a.sample,
+  location: "#{address[:street_number]} #{address[:street_name]}, #{address[:zipcode]} #{address[:city]}",
   user: dareth
 )
 
@@ -123,6 +127,7 @@ t2 = Task.create!(
   action: Task::ACTIONS.sample,
   status: 2, # Done
   price: (5..50).to_a.sample,
+  location: "#{address[:street_number]} #{address[:street_name]}, #{address[:zipcode]} #{address[:city]}",
   user: dareth
 )
 
@@ -130,6 +135,7 @@ t3 = Task.create!(
   action: Task::ACTIONS.sample,
   status: 0, # To do
   price: (5..50).to_a.sample,
+  location: "#{address[:street_number]} #{address[:street_name]}, #{address[:zipcode]} #{address[:city]}",
   user: dareth
 )
 
@@ -137,6 +143,7 @@ t4 = Task.create!(
   action: Task::ACTIONS.sample,
   status: 0, # To do
   price: (5..50).to_a.sample,
+  location: "#{address[:street_number]} #{address[:street_name]}, #{address[:zipcode]} #{address[:city]}",
   user: dareth
 )
 
@@ -144,6 +151,7 @@ t5 = Task.create!(
   action: Task::ACTIONS.sample,
   status: 0, # To do
   price: (5..50).to_a.sample,
+  location: "#{address[:street_number]} #{address[:street_name]}, #{address[:zipcode]} #{address[:city]}",
   user: dareth
 )
 
