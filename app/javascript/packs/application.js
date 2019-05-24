5a02186ee7cafff9df467eb82fd9cc3cf0730467
Dareth NHANG <DSGND@users.noreply.github.com>
2019-05-24 15:16:06 +0200

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,16 @@
+// BOOTSTRAP
 import "bootstrap";
 
+// MAPBOX
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
-
 initMapbox()
 initAutocomplete()
-
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
+// FLATPICKR
 import flatpickr from "flatpickr";
-
 // const flatpickr = require("flatpickr");
 // var datepicker = document.querySelector(".datepicker")
 // console.log(datepicker)
-
 flatpickr(".datepicker")
